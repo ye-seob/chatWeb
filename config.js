e3b0a67aa31923242728de692950c1de32eb2c1d
yeseob <jakalob103@naver.com>
2024-04-30 03:27:44 +0900

    최초 깃 저장

---
+++ config.js
@@ -0,0 +1,31 @@
+const { name } = require("ejs");
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/chat");
+
+connect
+  .then(() => {
+    console.log("DB 연결 성공");
+  })
+  .catch(() => {
+    console.log("DB 연결 실패");
+  });
+
+//스키마
+const loginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  id: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+const collection = new mongoose.model("users", loginSchema);
+
+module.exports = collection;
